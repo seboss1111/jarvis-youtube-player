7964540d538d36cfc2065c81e288ea394bdfbf3a
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-05 14:08:37 +0200

    plugin: Add verbosity in youtube-dl call

--- functions.sh
+++ functions.sh
@@ -7,7 +7,7 @@ jv_pg_yp_play_song()
 {
   # Download the video and play the song
   if [[ $2 =~ "True" ]]; then
-    youtube-dl --max-downloads 1 --format mp4 --output "/tmp/$1_audio.mp4" "gvsearch1:$1" && mplayer -novideo -ao alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_audio.mp4" && rm "/tmp/$1_audio.mp4" &
+    youtube-dl --verbose --max-downloads 1 --format mp4 --output "/tmp/$1_audio.mp4" "gvsearch1:$1" && mplayer -novideo -ao alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_audio.mp4" && rm "/tmp/$1_audio.mp4" &
   else
     nohup youtube-dl --max-downloads 1 --format mp4 --output "/tmp/$1_audio.mp4" "gvsearch1:$1" && mplayer -novideo alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_audio.mp4" && rm "/tmp/$1_audio.mp4" >/dev/null 2>/dev/stdout &
   fi
@@ -26,7 +26,7 @@ jv_pg_yp_play_video()
 
   # Download the video and play it
   if [[ $2 =~ "True" ]]; then
-    youtube-dl --max-downloads 1 --format mp4 --output "/tmp/$1_video.mp4" "gvsearch1:$1" && mplayer $full_screen -vf scale -zoom -ao alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_video.mp4" && rm "/tmp/$1_video.mp4" &
+    youtube-dl --verbose --max-downloads 1 --format mp4 --output "/tmp/$1_video.mp4" "gvsearch1:$1" && mplayer $full_screen -vf scale -zoom -ao alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_video.mp4" && rm "/tmp/$1_video.mp4" &
   else
     nohup youtube-dl --max-downloads 1 --format mp4 --output "/tmp/$1_video.mp4" "gvsearch1:$1" && mplayer $full_screen -vf scale -zoom alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_video.mp4" && rm "/tmp/$1_video.mp4" >/dev/null 2>/dev/stdout &
   fi
