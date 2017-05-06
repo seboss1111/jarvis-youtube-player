b2cf405e825814a8cc5ef84a6b42d782b361ae61
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-06 15:11:46 +0200

    plugin: Improve debug configuration

--- functions.sh
+++ functions.sh
@@ -2,20 +2,20 @@
 
 # Play a song from Youtube
 # $1 (string): Video name (example: "Eminem Lose it")
-# $2 (bool, optional): Verbose ("True" for Jarvis response, "False" or no value for no Jarvis response)
 jv_pg_yp_play_song()
 {
   # Download the video and play the song
-  if [[ $2 =~ "True" ]]; then
+  if [[ $var_jv_pg_yp_verbose =~ "True" ]]; then
     youtube-dl --verbose --max-downloads 1 --format mp4 --output "/tmp/$1_audio.mp4" "gvsearch1:$1" && mplayer -novideo -ao alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_audio.mp4" && rm "/tmp/$1_audio.mp4" &
   else
     nohup youtube-dl --max-downloads 1 --format mp4 --output "/tmp/$1_audio.mp4" "gvsearch1:$1" && mplayer -novideo alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_audio.mp4" && rm "/tmp/$1_audio.mp4" >/dev/null 2>/dev/stdout &
   fi
+
+  return 1
 }
 
 # Play a video from Youtube
 # $1 (string): Video name (example: "Eminem Lose it")
-# $2 (bool, optional): Verbose ("True" for Jarvis response, "False" or no value for no Jarvis response)
 jv_pg_yp_play_video()
 {
   # Check if showing video as full screen or not
@@ -25,9 +25,11 @@ jv_pg_yp_play_video()
   fi
 
   # Download the video and play it
-  if [[ $2 =~ "True" ]]; then
+  if [[ $var_jv_pg_yp_verbose =~ "True" ]]; then
     youtube-dl --verbose --max-downloads 1 --format mp4 --output "/tmp/$1_video.mp4" "gvsearch1:$1" && mplayer $full_screen -vf scale -zoom -ao alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_video.mp4" && rm "/tmp/$1_video.mp4" &
   else
     nohup youtube-dl --max-downloads 1 --format mp4 --output "/tmp/$1_video.mp4" "gvsearch1:$1" && mplayer $full_screen -vf scale -zoom alsa:device=hw=$var_jv_pg_yp_audio_card.$var_jv_pg_yp_audio_peripheral "/tmp/$1_video.mp4" && rm "/tmp/$1_video.mp4" >/dev/null 2>/dev/stdout &
   fi
+
+  return 1
 }
