aaecc82b65ef2145857b918939b059376f47abfd
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-17 10:59:16 +0200

    install: Improve youtube-dl install and upgrade

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+
+# Install pip (needed to install and upgrade youtube video downloader)
+[[ -z $(which python-pip) ]] && sudo apt-get --yes --force-yes install python-pip
+[[ -z $(which python3-pip) ]] && sudo apt-get --yes --force-yes install python3-pip
+
 # Install Youtube video downloader (Youtube-dl)
 [[ -z $(which youtube-dl) ]] && sudo add-apt-repository ppa:rvm/smplayer -y ; sudo apt-get update ; sudo apt-get --yes --force-yes install youtube-dl
+
+# Upgrade Youtube video downloader (youtube can change its way of working... this upgrade may be needed sometimes even after the install)
+sudo pip install --upgrade youtube-dl
+sudo pip3 install --upgrade youtube-dl
 
 # Install music player
 [[ -z $(which mplayer) ]] && sudo apt-get --yes --force-yes install mplayer
