6f0fc5b635a9fca649d108bd4a7d17d1059b0ed7
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-17 10:38:56 +0200

    install: Be more robust on youtube-dl install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Youtube video downloader (Youtube-dl)
-[[ -z $(which youtube-dl) ]] && sudo add-apt-repository ppa:rvm/smplayer -y && sudo apt-get update && sudo apt-get --yes --force-yes install youtube-dl
+[[ -z $(which youtube-dl) ]] && sudo add-apt-repository ppa:rvm/smplayer -y ; sudo apt-get update ; sudo apt-get --yes --force-yes install youtube-dl
 
 # Install music player
 [[ -z $(which mplayer) ]] && sudo apt-get --yes --force-yes install mplayer
